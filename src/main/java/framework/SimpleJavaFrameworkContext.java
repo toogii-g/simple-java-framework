358679302b4210ff7864eb73515c7c7bb5592819
bitoogii <bitoogii@yahoo.com>
2024-08-08 18:06:26 -0500

    initial commit

---
+++ src/main/java/framework/SimpleJavaFrameworkContext.java
@@ -0,0 +1,56 @@
+package framework;
+
+import org.reflections.Reflections;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Simple Java Framework
+ */
+public class SimpleJavaFrameworkContext {
+
+    private final Map<Class<?>, Object> beans = new HashMap<>();
+
+    public void start(Class<?> clazz) throws Exception{
+        instantiateClasses(clazz);
+        doDependencyInjection();
+
+    }
+
+    private void instantiateClasses(Class<?> clazz){
+        scanServices(clazz);
+    }
+
+    private void scanServices(Class<?> clazz){
+        try {
+            Reflections reflections = new Reflections(clazz.getPackageName());
+            Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+            for (Class<?> serviceClass : serviceClasses) {
+                System.out.println(serviceClass.getName());
+                Object obj = serviceClass.getDeclaredConstructor().newInstance();
+                beans.put(serviceClass, obj);
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void doDependencyInjection() throws IllegalAccessException{
+        for (Object bean : beans.values()) {
+            for (Field field : bean.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(Autowired.class)) {
+                    field.setAccessible(true);
+                    Object dependency = beans.get(field.getType());
+                    field.set(bean, dependency);
+                }
+            }
+        }
+    }
+
+    public <T> T getBean(Class<T> clazz) {
+        return (T) beans.get(clazz);
+    }
+}
