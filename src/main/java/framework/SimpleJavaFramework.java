cda6177334c77f3f19e4f9bf094c3c45b5f22c11
bitoogii <bitoogii@yahoo.com>
2024-08-14 12:15:09 -0500

    fix thread duplication

--- src/main/java/framework/SimpleJavaFramework.java
+++ src/main/java/framework/SimpleJavaFramework.java
@@ -5,10 +5,6 @@ import framework.util.Runnable;
 public class SimpleJavaFramework {
     public static SimpleJavaFrameworkContext run(Class<?> applicationClass, String... args) throws Exception {
         SimpleJavaFrameworkContext context = new SimpleJavaFrameworkContext(applicationClass);
-        Object applicationInstance = context.getBean(applicationClass);
-
-        Runnable runnable= (Runnable) applicationInstance;
-        runnable.run(args);
 
         return context;
     }
